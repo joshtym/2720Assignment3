3394119d1f6980245f9b725772c7ebd941a58a3c
Josh Tymburski <josh.tymburski@uleth.ca>
2014-02-21 20:40:24 -0700

    Second Iteration. Reformatted to be more suitable for developing

--- PostfixExpr.h
+++ PostfixExpr.h
@@ -4,26 +4,27 @@
 #include <string>
 #include "Tokenizer.h"
 
-
-/// Responsibility: evaluating postfix expressions.
+/// Reponsibility: Evaluating postfix expressions.
 /**
-Expressions consist only of literal fractional values and the four
-arithmetic operators.
- */
-class PostfixExpr {
- public:
-
-  /// Evaluates a postfix expression
-  /** Evaluates a postfix expression consisting only of positive
-      values and the four operators + - / *.
-      \param expr is the string containing the expression to be
-      evaluated.
-      \return the value of the expression
-      \throws SyntaxError if the expression is mal formed.
-      \throws LexicalError if the expression contains illegal characters.
-   */
-  double evaluate(std::string expr);
+ * Expressions consist only of literal fractional values and the four
+ * arithmetic operators.
+**/
+class PostfixExpr
+{
+	public:
+	
+		/// Evaluates a postfix expression
+		/**
+		 * Evaluates a postfix expression consisting only of positive
+		 * values and the four operators '+', '-', '/', and '*'.
+		 * \param expr is the string containing the expression to be
+		 * evaluated
+		 * \return the value of the expression
+		 * \throws SyntaxError if the expression is mal formed.
+		 * \throws LexicalError if the expression contains illegal
+		 * characters
+		**/
+		double evaluate(std::string expr);
 };
-
 
 #endif
