48f5e51432f63511ebc506e66ccdc708fd496b53
Josh Tymburski <josh.tymburski@uleth.ca>
2014-02-25 21:43:04 -0700

    10th iteration. Completed test by adding a successful return statement

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -5,6 +5,8 @@ void Tokenizer::parse(const std::string str)
 
 Tokenizer::Tokens Tokenizer::crtTokenType()
 {
+	// Obvoiusly not the final result
+	return Tokens::LITERAL;
 }
 
 Tokenizer::TokenValue Tokenizer::crtTokenValue()
