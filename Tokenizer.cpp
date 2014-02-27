94902bfdd101e7e41687b6877e080f8696a38a2a
Josh Tymburski <josh.tymburski@uleth.ca>
2014-02-27 16:23:52 -0700

    18th iteration. Revamp of Tokenizer mainly complete. Tests need to be rewritten to coincide with new Tokenizer

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -1,5 +1,6 @@
 #include "Tokenizer.h"
 #include <iostream>
+#include <cmath>
 
 int Tokenizer::count = 0;
 
@@ -25,7 +26,10 @@ Tokenizer::Tokens Tokenizer::crtTokenType(const std::string str)
 		return Tokens::OPERATOR;
 	else if (str[0] == '\n')
 		return Tokens::THEEND;
-	else if (str[0] == '0' || str[0] == '1' || str[0] == '2' || str[0] == '3' || str[0] == '4' || str[0] == '5' || str[0] == '6' || str[0] == '7' || str[0] == '8' || str[0] == '9')
+	else if (str[0] == '0' || str[0] == '1' || str[0] == '2' || 
+			  str[0] == '3' || str[0] == '4' || str[0] == '5' || 
+			  str[0] == '6' || str[0] == '7' || str[0] == '8' || 
+			  str[0] == '9')
 		return Tokens::LITERAL;
 	else
 		return Tokens::UNKNOWN;
@@ -45,9 +49,13 @@ Tokenizer::TokenValue Tokenizer::crtTokenValue(const std::string str)
 			return TokenValue(OperatorValue::SLASH);
 	}
 	else if (crtTokenType(str) == Tokens::LITERAL)
-		return TokenValue(convertStringToDouble(str);
-	else if (crtTokenType(str) == Tokens::UNKNOWN)
+		return TokenValue(convertStringToDouble(str));
+	else if (crtTokenType(str) == Tokens::UNKNOWN);
+		return TokenValue();
 	else
+		return TokenValue();
+	
+	
 }
 
 double Tokenizer::convertStringToDouble(const std::string str)
@@ -55,7 +63,10 @@ double Tokenizer::convertStringToDouble(const std::string str)
 	isFloatingPoint = false;
 	
 	double value = 0;
-	for (int i = 0; i < str.length(); i++)
+	int decimalIndex = str.length();
+	int tracker = 1;
+	
+	for (int i = 0; i < decimalIndex; i++)
 	{
 		switch (str[i])
 		{
@@ -88,12 +99,60 @@ double Tokenizer::convertStringToDouble(const std::string str)
 				break;
 			case '9':
 				value = value * 10 + 9;
+			case '.':
+				decimalIndex = i;
+				isFloatingNumber = true;
 				break;
 			default:
-				isFloatingPoint = true;
 				break;
 		}
 	}
+	
+	if (isFloatingNumber)
+	{
+		for (int j = decimalIndex + 1; j < str.length(); j++)
+		{
+			switch (str[i])
+			{
+				case '1':
+					value = value + (1 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '2':
+					value = value + (2 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '3':
+					value = value + (3 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '4':
+					value = value + (4 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '5':
+					value = value + (5 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '6':
+					value = value + (6 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '7':
+					value = value + (7 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '8':
+					value = value + (8 / (pow(10, tracker)));
+					tracker++;
+					break;
+				case '9':
+					value = value + (9 / (pow(10, tracker)));
+					tracker++;
+					break;
+				default:
+					tracker++;
+					break;
 	
 	return value;			
 }
